205b6be0ca9b479bbd6593a74b69d3aae97941ed
Hasib Mahmud <kishorpasha870@yahoo.com>
2014-12-11 14:30:34 +0600

    Create time-ago_component.js

---
+++ js/component/time-ago_component.js
@@ -0,0 +1,12 @@
+Exyht.TimeAgoComponent = Ember.Component.extend({
+  timeAgo: '',
+  
+  clock: function() {
+  	timezone = jstz.determine();
+  	timezoneName = timezone.name();
+    var newTimeAgo =  moment.tz(this.get('createdAt'), timezoneName).fromNow();
+    this.set('timeAgo', newTimeAgo);
+    
+    Ember.run.later(this, this.clock, 1000 * 60);
+  }.on('didInsertElement') 
+});
